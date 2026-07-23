f861487e54fc96c2c9ed2cbeddfc503eeeb5649d
hummeljadon-crypto <hummeljadon@gmail.com>
2026-07-23 13:25:32 +0200

    Delete main.ts

--- main.ts
+++
@@ -1,4 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-  <rect width="64" height="64" rx="16" fill="#061325"/>
-  <path d="M32 52S10 39 10 23c0-8 6-13 13-13 5 0 8 3 9 6 2-3 5-6 10-6 7 0 12 5 12 13 0 16-22 29-22 29Z" fill="#d7f7ff"/>
-</svg>
